73f9eeb48f05f94756034d24e713000978459488
Tsuyoshi Akiyama <ka090824@outlook.com>
2017-10-19 07:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,8 @@ function removeLastKitten() {
   var a = kittens.slice(0, kittens.length - 1)
   return a
 }
+
+function removeFirstKitten() {
+  var a = kittens.slice(1, kittens.length - 1)
+  return a
+}
